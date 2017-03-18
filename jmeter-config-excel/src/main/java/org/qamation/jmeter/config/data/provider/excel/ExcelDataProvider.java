8528b0530d4891b98b9ea0eb95d78995197a00e8
Pavel Gouchtchine <pavel.gouchtchine@cantire.com>
2017-03-18 17:58:11 -0400

    added example jmx file

--- jmeter-config-excel/src/main/java/org/qamation/jmeter/config/data/provider/excel/ExcelDataProvider.java
+++ jmeter-config-excel/src/main/java/org/qamation/jmeter/config/data/provider/excel/ExcelDataProvider.java
@@ -6,33 +6,48 @@ import org.apache.jmeter.engine.event.LoopIterationEvent;
 import org.apache.jmeter.engine.event.LoopIterationListener;
 import org.apache.jmeter.testbeans.TestBean;
 import org.apache.jmeter.testelement.TestStateListener;
-
+import org.apache.log.Logger;
+import org.apache.jorphan.logging.LoggingManager;
 /**
  * Created by Pavel.Gouchtchine on 03/10/2017.
  */
 public class ExcelDataProvider extends ConfigTestElement implements TestBean, LoopIterationListener, TestStateListener {
+    private static final Logger log = LoggingManager.getLoggerForClass();
+    private String excelFileName;
+
     @Override
     public void iterationStart(LoopIterationEvent loopIterationEvent) {
+        log.info("Iteration started with event:\n"+loopIterationEvent.toString());
 
     }
 
     @Override
     public void testStarted() {
-
+        log.info("Test Started");
     }
 
     @Override
     public void testStarted(String s) {
-
+        log.info("Parameter: "+s);
+        testStarted();
     }
 
     @Override
     public void testEnded() {
-
+        log.info("Test Ended.");
     }
 
     @Override
     public void testEnded(String s) {
+        log.info("Parameter: "+s);
+        testEnded();
+    }
 
+    public String getExcelFileName() {
+        return excelFileName;
+    }
+
+    public void setExcelFileName(String excelFileName) {
+        this.excelFileName = excelFileName;
     }
 }
--- jmeter-config-excel/src/main/java/org/qamation/jmeter/config/data/provider/excel/ExcelDataProviderBeanInfo.java
+++ jmeter-config-excel/src/main/java/org/qamation/jmeter/config/data/provider/excel/ExcelDataProviderBeanInfo.java
@@ -3,11 +3,22 @@ package org.qamation.jmeter.config.data.provider.excel;
 import org.apache.jmeter.testbeans.BeanInfoSupport;
 import org.apache.jmeter.testbeans.TestBean;
 
+import java.beans.PropertyDescriptor;
+
 /**
  * Created by Pavel.Gouchtchine on 03/10/2017.
  */
 public class ExcelDataProviderBeanInfo extends BeanInfoSupport {
-    protected ExcelDataProviderBeanInfo(Class<? extends TestBean> beanClass) {
+    private static final String GROUP_FILE_DATA ="excel_file_data";
+    private static final String EXCELFILENAME = "excelFileName";
+
+    public ExcelDataProviderBeanInfo(Class<? extends TestBean> beanClass) {
         super(beanClass);
+        createPropertyGroup("excel_file_data",
+                new String[] {EXCELFILENAME});
+        PropertyDescriptor p = property(EXCELFILENAME);
+        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+        p.setValue(DEFAULT,"<ENTER FILE NAME HERE>");
+        p.setValue(NOT_EXPRESSION, Boolean.TRUE);
     }
 }
