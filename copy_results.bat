b7b89a3adb1eb981736b017447d7d90afe9a8575
gpawel <gpawel@gmail.com>
2017-03-24 18:56:55 -0400

    changed copy_results.bat

--- copy_results.bat
+++ copy_results.bat
@@ -1,6 +1,8 @@
 rem echo off
-set TARGET=C:\TEST_ENV\apache-jmeter-3.1\lib\ext  
-set ARCH=C:\TEST_ENV\SMOKE_TESTS\REQUIRED_FILES\for_jmeter_lib_ext
+set TARGET_ROOT=D:\QA_ENV_1
+set TARGET_JMETER_FOLDER=Jmeter
+set TARGET=%TARGET_ROOT%\%TARGET_JMETER_FOLDER%\lib\ext  
+REM set ARCH=C:\TEST_ENV\SMOKE_TESTS\REQUIRED_FILES\for_jmeter_lib_ext
 call copy /y .\excel-data-privider\target\*.jar build
 call copy /y .\keyboard\target\*.jar build
 call copy /y .\navigator\target\*.jar build
@@ -15,7 +17,7 @@ call copy /y .\jmeter-java-sampler-web-extension\target\*.jar build
 call copy /y .\jmeter-java-sampler-wmq\target\*.jar build
 call copy /y .\jmeter-config-excel\target\*.jar build
 call copy build\*.jar %TARGET%
-call copy build\*.jar %ARCH%
+REM call copy build\*.jar %ARCH%
 rem copy /y extentions*.jar c:\TEST_ENV\apache-jmeter-3.1\lib\ext
 rem copy /y extentions*.jar c:\TEST_ENV\SMOKE_TESTS\REQUIRED_FILES\for_jmeter_lib_ext
 rem cd ..
