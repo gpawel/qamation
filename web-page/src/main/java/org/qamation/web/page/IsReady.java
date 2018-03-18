b96f1c2756d31661ed53fd0daf410f0f131ce1d6
Pavel Gouchtchine <pavel.gouchtchine@cantire.com>
2018-03-18 12:19:20 -0400

    Refactored isPageReady. added isPageReady(ExpectedCondition c) method. Updated corresponding classes. Added TimeOutConfig variables to support WebDriverWait timeout and interval parameters using outside properties.

--- web-page/src/main/java/org/qamation/web/page/IsReady.java
+++ web-page/src/main/java/org/qamation/web/page/IsReady.java
@@ -1,5 +1,8 @@
 package org.qamation.web.page;
 
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
 public interface IsReady {
 	public boolean isReady();
+	public boolean isReady(ExpectedCondition condition);
 }
