3e5ab6170d90a26b0231e60fea71df5437153c8d
pavel.gouchtchine <pavel.gouchtchine@cantire.com>
2019-10-16 13:24:49 -0400

    Refactoring WebDriverFactory. Extracted Chrome related functions and moved them into separated class.

---
+++ src/main/java/org/qamation/webdriver/utils/ChromeDriverFactory.java
@@ -0,0 +1,68 @@
+package org.qamation.webdriver.utils;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.qamation.utils.ResourceUtils;
+
+import java.net.URL;
+
+public class ChromeDriverFactory {
+    public static ChromeDriver createChromeDriver(String pathToExec) {
+        System.setProperty("webdriver.chrome.driver",pathToExec);
+        ChromeOptions chromeOptions = getChromeOptions();
+        return new ChromeDriver(chromeOptions);
+    }
+
+    public static ChromeDriver createChromeDriver() {
+        String pathToExec = getChromeDriverPath();
+        return createChromeDriver(pathToExec);
+    }
+
+    public static WebDriver createRemoteChromeDriver(URL hub) {
+        ChromeOptions chromeOptions = getChromeOptions();
+        RemoteWebDriver driver = WebDriverFactory.createRemoteWebdriver(hub, new DesiredCapabilities(chromeOptions));
+        return driver;
+    }
+
+    public static String getChromeDriverPath() {
+        String defValue = System.getProperty("user.dir")+"/Selenium/ChromeDriver/chromedriver";
+        return ResourceUtils.getSystemProperty("webdriver.chrome.driver",defValue);
+    }
+
+    public static ChromeOptions getChromeOptions() {
+        MutableCapabilities mc = WebDriverFactory.getDefaultMutualCapabilities();
+        return getChromeOptionsFrom(mc);
+    }
+
+    private static ChromeOptions getChromeOptionsFrom(MutableCapabilities mc) {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.merge(mc);
+        chromeOptions = chromeOptions.addArguments("--disable-extensions");
+        return chromeOptions;
+    }
+
+    private static DesiredCapabilities setChromeCapabilities() {
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        ChromeOptions options = new ChromeOptions();
+        options = options.addArguments("--disable-extensions");
+        dc.setCapability(ChromeOptions.CAPABILITY, options);
+        return dc;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        WebDriver driver = WebDriverFactory.createChromeWebDriver("C:\\workspace\\FRONTIER-TESTS\\resources\\driver\\chromedriver.exe");
+        driver.get("https://google.com");
+        Actions action = new Actions(driver);
+        action.sendKeys("qa automation").build().perform();
+        action.pause(1000);
+        action.sendKeys(Keys.ENTER).build().perform();
+        action.pause(1000);
+
+    }
+}
